1b2111c19531457c6cbbbf3ff7e295ddf7b956b2
Leonardo Oliveira <leonardogbrl85@gmail.com>
2022-07-21 23:28:11 -0300

    Algumas configurações do app

---
+++ app/src/main/java/com/github/leonardogabrielmg/simulador/MainActivity.java
@@ -0,0 +1,22 @@
+package com.github.leonardogabrielmg.simulador;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.leonardogabrielmg.simulador.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
